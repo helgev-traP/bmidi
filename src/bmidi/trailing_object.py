1d498dc666f7d809c31817f64162a9ed2aa94114
helgev-traP <113248435+helgev-traP@users.noreply.github.com>
2023-10-23 23:36:28 +0900

    log

--- src/bmidi/trailing_object.py
+++ src/bmidi/trailing_object.py
@@ -44,58 +44,58 @@ class SimpleObject:
         self, name, mesh, start_from: BasicEndPoint, end_to: BasicEndPoint
     ) -> None:
         # クラスの構造体を引数に取れるようにする
-        self.object = bpy.data.objects.new(name, mesh)
-        self.start_from = start_from
-        self.end_to = end_to
-        self.bake_to_blender(self.start_from, self.end_to)
+        self.__object = bpy.data.objects.new(name, mesh)
+        self.__start_from = start_from
+        self.__end_to = end_to
+        self.bake_to_blender(self.__start_from, self.__end_to)
 
     def bake_to_blender(self, start_from: BasicEndPoint, end_to: BasicEndPoint):
         """Blenderに焼く"""
-        self.object.data.materials[0].use_nodes = True
+        self.__object.data.materials[0].use_nodes = True
         # hide object
         bpy.context.scene.frame_set(start_from.frame - 1)
-        self.object.hide_render = True
-        self.object.hide_viewport = True
-        self.object.keyframe_insert(data_path="hide_render", index=-1)
-        self.object.keyframe_insert(data_path="hide_viewport", index=-1)
+        self.__object.hide_render = True
+        self.__object.hide_viewport = True
+        self.__object.keyframe_insert(data_path="hide_render", index=-1)
+        self.__object.keyframe_insert(data_path="hide_viewport", index=-1)
         # start point
         bpy.context.scene.frame_set(start_from.frame)
-        self.object.location = start_from.location
-        self.object.scale = start_from.scale
-        self.object.rotation_euler = start_from.euler
-        self.object.data.materials[0].node_tree.nodes["Principled BSDF"].inputs[
+        self.__object.location = start_from.location
+        self.__object.scale = start_from.scale
+        self.__object.rotation_euler = start_from.euler
+        self.__object.data.materials[0].node_tree.nodes["Principled BSDF"].inputs[
             21
         ].default_value = start_from.alpha
-        self.object.hide_render = False
-        self.object.hide_viewport = False
+        self.__object.hide_render = False
+        self.__object.hide_viewport = False
         # Bake
-        self.object.keyframe_insert(data_path="location", index=-1)
-        self.object.keyframe_insert(data_path="scale", index=-1)
-        self.object.keyframe_insert(data_path="rotation_euler", index=-1)
-        self.object.data.materials[0].node_tree.nodes["Principled BSDF"].inputs[
+        self.__object.keyframe_insert(data_path="location", index=-1)
+        self.__object.keyframe_insert(data_path="scale", index=-1)
+        self.__object.keyframe_insert(data_path="rotation_euler", index=-1)
+        self.__object.data.materials[0].node_tree.nodes["Principled BSDF"].inputs[
             21
         ].keyframe_insert(data_path="default_value", index=-1)
-        self.object.keyframe_insert(data_path="hide_render", index=-1)
-        self.object.keyframe_insert(data_path="hide_viewport", index=-1)
+        self.__object.keyframe_insert(data_path="hide_render", index=-1)
+        self.__object.keyframe_insert(data_path="hide_viewport", index=-1)
         # end point
         bpy.context.scene.frame_set(end_to.frame)
-        self.object.location = end_to.location
-        self.object.scale = end_to.scale
-        self.object.rotation_euler = end_to.euler
-        self.object.data.materials[0].node_tree.nodes["Principled BSDF"].inputs[
+        self.__object.location = end_to.location
+        self.__object.scale = end_to.scale
+        self.__object.rotation_euler = end_to.euler
+        self.__object.data.materials[0].node_tree.nodes["Principled BSDF"].inputs[
             21
         ].default_value = end_to.alpha
-        self.object.hide_render = True
-        self.object.hide_viewport = True
+        self.__object.hide_render = True
+        self.__object.hide_viewport = True
         # Bake
-        self.object.keyframe_insert(data_path="location", index=-1)
-        self.object.keyframe_insert(data_path="scale", index=-1)
-        self.object.keyframe_insert(data_path="rotation_euler", index=-1)
-        self.object.data.materials[0].node_tree.nodes["Principled BSDF"].inputs[
+        self.__object.keyframe_insert(data_path="location", index=-1)
+        self.__object.keyframe_insert(data_path="scale", index=-1)
+        self.__object.keyframe_insert(data_path="rotation_euler", index=-1)
+        self.__object.data.materials[0].node_tree.nodes["Principled BSDF"].inputs[
             21
         ].keyframe_insert(data_path="default_value", index=-1)
-        self.object.keyframe_insert(data_path="hide_render", index=-1)
-        self.object.keyframe_insert(data_path="hide_viewport", index=-1)
+        self.__object.keyframe_insert(data_path="hide_render", index=-1)
+        self.__object.keyframe_insert(data_path="hide_viewport", index=-1)
 
 
 # # ExtendedObject
