817ee41a59310a2152741e8ad63f24b3c59a19d4
Helgev <113248435+helgev-traP@users.noreply.github.com>
2023-10-23 08:35:47 +0000

    done!単純なオブジェクトの移動だけのクラスを作った

--- src/bmidi/trailing_object.py
+++ src/bmidi/trailing_object.py
@@ -9,48 +9,93 @@
 
 import bpy
 
-class SimpleObject():
-    '''2点間の移動を簡単にいい感じにできる。宣言した瞬間にBlenderに焼き込んじゃう'''
-    class EndPoint():
-        '''関数間の受け渡しでいい感じにレベル分けするためのもの'''
-        # クラス名あとでいい感じにする
-        def __init__(self,
-                     frame: int,
-                     location: list[float],
-                     scale: list[float],
-                     euler: list[float],
-                     alpha: float
-                     ) -> None:
-            self.frame = frame
-            self.location = location
-            self.scale = scale
-            self.euler = euler
-            self.alpha = alpha
-            return
 
-    def __init__(self, name, mesh, start_from: EndPoint, end_to:EndPoint) -> None:
+class EndPoint():
+    '''関数間の受け渡しでいい感じにレベル分けするためのもの'''
+    # クラス名あとでいい感じにする
+
+    def __init__(self,
+                 frame: int,
+                 location: list[float],
+                 scale: list[float],
+                 euler: list[float],
+                 alpha: float
+                 ) -> None:
+        self.frame = frame
+        self.location = location
+        self.scale = scale
+        self.euler = euler
+        self.alpha = alpha
+        return
+
+
+class SimpleObjectClass():
+    '''2点間の移動を簡単にいい感じにできる。宣言した瞬間にBlenderに焼き込んじゃう'''
+
+    def __init__(self, name, mesh, start_from: EndPoint, end_to: EndPoint) -> None:
         # クラスの構造体を引数に取れるようにする
         self.object = bpy.data.objects.new(name, mesh)
         self.start_from = start_from
         self.end_to = end_to
-        self.bake_to_blender(self.start_from,self.end_to)
+        self.bake_to_blender(self.start_from, self.end_to)
 
-    def end_point(self, frame, location, scale, euler, alpha):
-        '''関数間の受け渡しでいい感じにレベル分けするためのもの'''
-        point = self.EndPoint(frame, location, scale, euler, alpha)
-        return point
-
-    def bake_to_blender(self, start_from: EndPoint, end_to:EndPoint):
+    def bake_to_blender(self, start_from: EndPoint, end_to: EndPoint):
         '''Blenderに焼く'''
         self.object.data.materials[0].use_nodes = True
+        # hide object
+        bpy.context.scene.frame_set(start_from.frame - 1)
+        self.object.hide_render = True
+        self.object.hide_viewport = True
+        self.object.keyframe_insert(data_path="hide_render", index=-1)
+        self.object.keyframe_insert(data_path="hide_viewport", index=-1)
         # start point
         bpy.context.scene.frame_set(start_from.frame)
         self.object.location = start_from.location
         self.object.scale = start_from.scale
         self.object.rotation_euler = start_from.euler
         self.object.data.materials[0].node_tree.nodes["Principled BSDF"].inputs[21].default_value = start_from.alpha
+        self.object.hide_render = False
+        self.object.hide_viewport = False
         # Bake
         self.object.keyframe_insert(data_path="location", index=-1)
         self.object.keyframe_insert(data_path="scale", index=-1)
         self.object.keyframe_insert(data_path="rotation_euler", index=-1)
-        self.object.data.materials[0].node_tree.nodes["Principled BSDF"].inputs[21].keyframe_insert(data_path="default_value", index=-1)
+        self.object.data.materials[0].node_tree.nodes["Principled BSDF"].inputs[21].keyframe_insert(
+            data_path="default_value", index=-1)
+        self.object.keyframe_insert(data_path="hide_render", index=-1)
+        self.object.keyframe_insert(data_path="hide_viewport", index=-1)
+        # end point
+        bpy.context.scene.frame_set(end_to.frame)
+        self.object.location = end_to.location
+        self.object.scale = end_to.scale
+        self.object.rotation_euler = end_to.euler
+        self.object.data.materials[0].node_tree.nodes["Principled BSDF"].inputs[21].default_value = end_to.alpha
+        self.object.hide_render = True
+        self.object.hide_viewport = True
+        # Bake
+        self.object.keyframe_insert(data_path="location", index=-1)
+        self.object.keyframe_insert(data_path="scale", index=-1)
+        self.object.keyframe_insert(data_path="rotation_euler", index=-1)
+        self.object.data.materials[0].node_tree.nodes["Principled BSDF"].inputs[21].keyframe_insert(
+            data_path="default_value", index=-1)
+        self.object.keyframe_insert(data_path="hide_render", index=-1)
+        self.object.keyframe_insert(data_path="hide_viewport", index=-1)
+
+if __name__ == "__main__":
+    print("Here is sample program. Nothing will happen in CLI!")
+    sample = SimpleObjectClass(
+        name="sample",
+        mesh=bpy.data.meshes.new(),
+        start_from=EndPoint(frame=1,
+                            location=[1, 1, 1],
+                            scale=[1, 1, 1],
+                            euler=[0, 0, 0],
+                            alpha=1.0,
+                            ),
+        end_to=EndPoint(frame=1,
+                        location=[1, 1, 1],
+                        scale=[1, 1, 1],
+                        euler=[0, 0, 0],
+                        alpha=1.0,
+                        ),
+    )
