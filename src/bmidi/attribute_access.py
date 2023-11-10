eba4e52d01623f5608e668c756e722a97356e094
helgev-traP <113248435+helgev-traP@users.noreply.github.com>
2023-11-10 21:04:30 +0900

    fix: set/getattr_hバグ修正。close

--- src/bmidi/attribute_access.py
+++ src/bmidi/attribute_access.py
@@ -1,10 +1,10 @@
-'''wrapping get/setattr to access to inner class method.'''
+"""wrapping get/setattr to access to inner class method."""
 
 import re
 
-# path無しはgetのみ
+
 def getattr_h(instance, attribute_path: str):
-    '''wrap getattr'''
+    """wrap getattr"""
     # trail empty path
     if attribute_path == "":
         return instance
@@ -29,31 +29,24 @@ def getattr_h(instance, attribute_path: str):
 
 
 def setattr_h(instance, attribute_path: str, value):
-    '''wrap setattr'''
+    """wrap setattr"""
     attribute = attribute_path.split(".")
     for i in range(len(attribute) - 1):
         # handle head/end/doubled dot
-        if i != "":
-            if re.fullmatch(R"[a-zA-Z_]+", i) is not None:
-                instance = getattr(instance, i)
-            elif re.fullmatch(R"[a-zA-Z_]+\[[0-9]+\]", i) is not None:
-                instance = getattr(instance, re.split(R"[\[\]]", i)[0])
-                instance = instance[int(re.split(R"[\[\]]", i)[1])]
-            elif re.fullmatch(R"[a-zA-Z_]+\[[\'\"][a-zA-z_][\'\"]\]", i) is not None:
-                instance = getattr(instance, re.split(R"[\[\]]", i)[0])
-                instance_key = re.split(R"[\[\]]", i)[1]
+        if attribute[i] != "":
+            if re.fullmatch(R"[a-zA-Z_]+", attribute[i]) is not None:
+                instance = getattr(instance, attribute[i])
+            elif re.fullmatch(R"[a-zA-Z_]+\[[0-9]+\]", attribute[i]) is not None:
+                instance = getattr(instance, re.split(R"[\[\]]", attribute[i])[0])
+                instance = instance[int(re.split(R"[\[\]]", attribute[i])[1])]
+            elif (
+                re.fullmatch(R"[a-zA-Z_]+\[[\'\"][a-zA-z_][\'\"]\]", attribute[i])
+                is not None
+            ):
+                instance = getattr(instance, re.split(R"[\[\]]", attribute[i])[0])
+                instance_key = re.split(R"[\[\]]", attribute[i])[1]
                 instance_key = re.sub(R"[\'\"]", "", instance_key)
                 instance = instance[instance_key]
             else:
                 print("invalid path at function: setattr_h")
     setattr(instance, attribute[-1], value)
-
-if __name__ == "main":
-    class obj:
-        class iobj:
-            def __init__(self):
-                self.ver = 0
-        def __init__(self):
-            self.dic = {"a": self.iobj(),"b": self.iobj()}
-    a = obj()
-    a.
