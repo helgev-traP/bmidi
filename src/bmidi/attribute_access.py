7f0c97e71d4894b17413042bd98d228f34cbeb0e
Helgev <113248435+helgev-traP@users.noreply.github.com>
2023-11-10 08:26:04 +0000

    辞書に対応するも、バグ拭えず

--- src/bmidi/attribute_access.py
+++ src/bmidi/attribute_access.py
@@ -15,9 +15,16 @@ def getattr_h(instance, attribute_path: str):
         if i != "":
             if re.fullmatch(R"[a-zA-Z_]+", i) is not None:
                 instance = getattr(instance, i)
-            if re.fullmatch(R"[a-zA-Z_]+\[[0-9]+\]", i) is not None:
+            elif re.fullmatch(R"[a-zA-Z_]+\[[0-9]+\]", i) is not None:
                 instance = getattr(instance, re.split(R"[\[\]]", i)[0])
                 instance = instance[int(re.split(R"[\[\]]", i)[1])]
+            elif re.fullmatch(R"[a-zA-Z_]+\[[\'\"][a-zA-z_][\'\"]\]", i) is not None:
+                instance = getattr(instance, re.split(R"[\[\]]", i)[0])
+                instance_key = re.split(R"[\[\]]", i)[1]
+                instance_key = re.sub(R"[\'\"]", "", instance_key)
+                instance = instance[instance_key]
+            else:
+                print("invalid path at function: getattr_h")
     return instance
 
 
@@ -29,7 +36,24 @@ def setattr_h(instance, attribute_path: str, value):
         if i != "":
             if re.fullmatch(R"[a-zA-Z_]+", i) is not None:
                 instance = getattr(instance, i)
-            if re.fullmatch(R"[a-zA-Z_]+\[[0-9]+\]", i) is not None:
+            elif re.fullmatch(R"[a-zA-Z_]+\[[0-9]+\]", i) is not None:
                 instance = getattr(instance, re.split(R"[\[\]]", i)[0])
                 instance = instance[int(re.split(R"[\[\]]", i)[1])]
+            elif re.fullmatch(R"[a-zA-Z_]+\[[\'\"][a-zA-z_][\'\"]\]", i) is not None:
+                instance = getattr(instance, re.split(R"[\[\]]", i)[0])
+                instance_key = re.split(R"[\[\]]", i)[1]
+                instance_key = re.sub(R"[\'\"]", "", instance_key)
+                instance = instance[instance_key]
+            else:
+                print("invalid path at function: setattr_h")
     setattr(instance, attribute[-1], value)
+
+if __name__ == "main":
+    class obj:
+        class iobj:
+            def __init__(self):
+                self.ver = 0
+        def __init__(self):
+            self.dic = {"a": self.iobj(),"b": self.iobj()}
+    a = obj()
+    a.
