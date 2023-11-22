068eb2b044ca7a8c02508738f23621dd52d5cab1
helgev-traP <113248435+helgev-traP@users.noreply.github.com>
2023-11-22 22:56:20 +0900

    log

--- src/setup.py
+++ src/setup.py
@@ -1,4 +1,4 @@
-'''
+"""
 メジャーバージョン:
 v0:
 MIDIしか扱えない
@@ -6,13 +6,13 @@ v1:
 Pythonからある程度Blenderのオブジェクトが扱える
 v2:
 GUIで操作できるようにする。アドオン化を狙う
-'''
+"""
 
 from setuptools import setup, find_packages
 
 setup(
-    name='bmidi',
-    version='1.1.0',
+    name="bmidi",
+    version="1.1.0",
     packages=find_packages(),
     install_requires="mido",
 )
