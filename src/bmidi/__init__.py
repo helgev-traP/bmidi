0fc709a97f154fc1406596d690f5e968abf468a2
helgev-traP <113248435+helgev-traP@users.noreply.github.com>
2023-12-13 00:45:13 +0900

    log

--- src/bmidi/__init__.py
+++ src/bmidi/__init__.py
@@ -1,14 +1,18 @@
 # 開発環境のみ
 import sys
+
 sys.dont_write_bytecode = True
 # attr
 from attribute_access.attribute_access import *
+
 # object
 from object.trailing_object import *
+
 # score
 from score.from_score import *
 from score.midi_track import *
 from score.score import *
+
 # root
 from object_score import *
 from motion_cut import *
