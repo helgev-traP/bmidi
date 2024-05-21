00aefb66bd1e27e99f108ea8e31c0fe1bea204af
helgev <helgev22@gmail.com>
2024-05-21 12:10:28 +0900

    importエラーに困っている

--- src/bmidi/__init__.py
+++ src/bmidi/__init__.py
@@ -2,22 +2,25 @@
 import sys
 
 sys.dont_write_bytecode = True
+
+# ! importの際にエラー sandboxでテストする
+
 # attr
-from bmidi.attribute_access.attribute_access import getattr_h
-from bmidi.attribute_access.attribute_access import setattr_h
+from .bmidi.attribute_access.attribute_access import getattr_h
+from .bmidi.attribute_access.attribute_access import setattr_h
 
 # object
-from bmidi.object.trailing_object import SimpleObjectEndPoint
-from bmidi.object.trailing_object import SimpleObject
-from bmidi.object.trailing_object import Object
+from .bmidi.object.trailing_object import SimpleObjectEndPoint
+from .bmidi.object.trailing_object import SimpleObject
+from .bmidi.object.trailing_object import Object
 
 # score
-from bmidi.score.midi_track import *
-from bmidi.score.score import *
+from .bmidi.score.midi_track import *
+from .bmidi.score.score import *
 
 # root
-from bmidi.object_score import *
-from bmidi.motion_cut import *
+from .bmidi.object_score import *
+from .bmidi.motion_cut import *
 
 # help
-from bmidi.help_module import *
+from .bmidi.help_module import *
--- src/test.py
+++ src/test.py
@@ -2,23 +2,4 @@
 import sys
 
 sys.dont_write_bytecode = True
-import __init__ as bmidi
-
-#
-
-import mido
-
-midi_1 = mido.MidiFile(R"C:\0_program\projects\bmidi\test_midi\test_format_1.mid")
-
-midi_1_one_track = mido.merge_tracks(midi_1.tracks)
-
-print(midi_1_one_track)
-
-print("---")
-
-test = bmidi.Score(
-    name="test", filepath=R"C:\0_program\projects\bmidi\test_midi\test_format_1.mid"
-)
-
-for i in test.messages:
-    print(i.__dict__)
+import bmidi
